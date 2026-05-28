264694459ca9ec482f816bc8394061f5066d47dc
shivam-js <prajapati2shivam@gmail.com>
2026-05-28 16:19:16 +0530

    fix render resume upload folder

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,20 +1,26 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+const uploadDir = path.join(
+  __dirname,
+  "../uploads/resumes"
+);
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, {
+    recursive: true,
+  });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(
-    null,
-    path.join(
-        __dirname,
-        "../uploads/resumes"
-    )
-    );
+    cb(null, uploadDir);
   },
 
   filename: function (req, file, cb) {
